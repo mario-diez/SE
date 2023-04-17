5ea312a33e5e63f04f70243c4c3b48d0e8680acd
mario-diez <mario.diez@udc.es>
2023-04-17 12:40:07 +0200

    Primer apartado

--- main.c
+++ main.c
@@ -1,11 +1,72 @@
 #include "MKL46Z4.h"
+#include "lcd.h"
 
-int door_status1=0;
-int door_status2=0;
-int safe=0;   //0 es usafe 1 es safe
-int hasChanged=0; //Controla los cambios de estado
 
-void led_green_init()
+
+// LED (RG)
+// LED_GREEN = PTD5 (pin 98)
+// LED_RED = PTE29 (pin 26)
+
+// SWICHES
+// RIGHT (SW1) = PTC3 (pin 73)
+// LEFT (SW2) = PTC12 (pin 88)
+
+// Enable IRCLK (Internal Reference Clock)
+// see Chapter 24 in MCU doc
+void irclk_ini()
+{
+  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+}
+
+void delay(void)
+{
+  volatile int i;
+
+  for (i = 0; i < 1000000; i++);
+}
+
+// RIGHT_SWITCH (SW1) = PTC3
+void sw1_ini()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  GPIOC->PDDR &= ~(1 << 3);
+}
+
+// LEFT_SWITCH (SW2) = PTC12
+void sw2_ini()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  GPIOC->PDDR &= ~(1 << 12);
+}
+
+int sw1_check()
+{
+  return( !(GPIOC->PDIR & (1 << 3)) );
+}
+
+int sw2_check()
+{
+  return( !(GPIOC->PDIR & (1 << 12)) );
+}
+
+// RIGHT_SWITCH (SW1) = PTC3
+// LEFT_SWITCH (SW2) = PTC12
+void sws_ini()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  GPIOC->PDDR &= ~(1 << 3 | 1 << 12);
+}
+
+// LED_GREEN = PTD5
+void led_green_ini()
 {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
@@ -13,18 +74,14 @@ void led_green_init()
   GPIOD->PDDR |= (1 << 5);
   GPIOD->PSOR = (1 << 5);
 }
-void delay(void)
-{
-  volatile int i;
 
-  for (i = 0; i < 1000000; i++);
-}
 void led_green_toggle()
 {
   GPIOD->PTOR = (1 << 5);
 }
 
-void led_red_init()
+// LED_RED = PTE29
+void led_red_ini()
 {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
@@ -38,63 +95,56 @@ void led_red_toggle(void)
   GPIOE->PTOR = (1 << 29);
 }
 
-void initButtons(){
+// LED_RED = PTE29
+// LED_GREEN = PTD5
+void leds_ini()
+{
   SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] = PORT_PCR_MUX(1);
-  PORTC->PCR[3] |= PORT_PCR_PE(1) | PORT_PCR_PS(1);
-  PORTC->PCR[3] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-
-  PORTC->PCR[3] |= PORT_PCR_IRQC(0xA);
-
-  PORTC->PCR[12] = PORT_PCR_MUX(1);
-  PORTC->PCR[12] |= PORT_PCR_PE(1) | PORT_PCR_PS(1);
-  PORTC->PCR[12] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-
-  PORTC->PCR[12] |= PORT_PCR_IRQC(0xA);
-
-  NVIC_SetPriority(PORTC_PORTD_IRQn,3);
-  NVIC_EnableIRQ(PORTC_PORTD_IRQn);
-
+  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK;
+  PORTD->PCR[5] = PORT_PCR_MUX(1);
+  PORTE->PCR[29] = PORT_PCR_MUX(1);
+  GPIOD->PDDR |= (1 << 5);
+  GPIOE->PDDR |= (1 << 29);
+  // both LEDS off after init
+  GPIOD->PSOR = (1 << 5);
+  GPIOE->PSOR = (1 << 29);
 }
 
-void ledHandler(int safe, int hasChanged){
-  if(safe!=hasChanged){          
-    led_green_toggle();              
-    led_red_toggle();
-  }
-
-}
-
-void PORTDIntHandler(void){
-
-    if(!(PORTC->PCR[3] & PORT_PCR_ISF_MASK)){
-      door_status1=!door_status1;
-    }
-    if(!(PORTC->PCR[12] & PORT_PCR_ISF_MASK)){
-      door_status2=!door_status2;
-    }
-    if((door_status1==1 && door_status2==1) || (door_status1==1 && door_status2==0) || (door_status1==0 && door_status2==1)){
-      hasChanged=1;
-    }else{
-      hasChanged=0;
-    }
-    ledHandler(safe,hasChanged);
-    safe=hasChanged;
-
-    PORTC->PCR[3] |= PORT_PCR_ISF_MASK;
-    PORTC->PCR[12] |= PORT_PCR_ISF_MASK;
-
-}
+// Hit condition: (else, it is a miss)
+// - Left switch matches red light
+// - Right switch matches green light
 
 int main(void)
 {
-  initButtons();
-  led_green_init();
-  led_red_init();
-  led_green_toggle();
+  irclk_ini(); // Enable internal ref clk to use by LCD
+
+  lcd_ini();
+  lcd_display_dec(666);
+
+  // 'Random' sequence :-)
+  volatile unsigned int sequence = 0x32B14D98,
+    index = 0;
+
+  while (index < 32) {
+    if (sequence & (1 << index)) { //odd
+      //
+      // Switch on green led
+      // [...]
+      //
+    } else { //even
+      //
+      // Switch on red led
+      // [...]
+      //
+    }
+    // [...]
+  }
+
+  // Stop game and show blinking final result in LCD: hits:misses
+  // [...]
+  //
+
   while (1) {
-    
   }
 
   return 0;
